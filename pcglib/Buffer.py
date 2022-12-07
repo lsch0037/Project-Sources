64c4600c2de552806c7735abed24b982a95a8f7d
BuildTools <lukas.schimpf@gmail.com>
2022-12-07 14:04:20 +0100

    Fixed offset and restructured buffer

--- pcglib/Buffer.py
+++ pcglib/Buffer.py
@@ -2,49 +2,94 @@ import numpy as np
 from VectorOperations import Vector
 
 class Buffer():
-    def __init__(self, x_size, y_size, z_size, O=[0,0,0]):
-        self._arr = np.zeros((x_size,y_size,z_size))
-        self.setOrigin(O)
+    def __init__(self, x_0, y_0, z_0, x, y, z):
+        x_len = x - x_0
+        y_len = y - y_0
+        z_len = z - z_0
+        
+        self._x_0 = x_0
+        self._y_0 = y_0
+        self._z_0 = z_0
+
+        self._x_1 = x
+        self._y_1 = y
+        self._z_1 = z
+
+        self._arr = np.zeros((x_len, y_len, z_len))
 
     def set(self, x,y,z,id):
-        self._arr[x][y][z] = id
+        self._arr[x - self._x_0][y - self._y_0][z - self._z_0] = id
+
+    def resize(self, x, y, z):
+        x_len, y_len, z_len = self._arr.shape
+
+        new_x_0, new_y_0, new_z_0 = self._x_0, self._y_0, self._z_0
+        new_x, new_y, new_z = x_len, y_len, z_len
+
+
+        # if x greater than range
+        if x > self._x_0 + x_len:
+            new_x = x
+        elif x < self._x_0:
+            new_x_0 = x
+
+        # if y greater than range
+        if y > self._y_0 + y_len:
+            new_y = y
+        elif y < self._y_0:
+            new_y_0 = y
+
+        # if z greater than range
+        if z > self._z_0 + z_len:
+            new_z = z
+        elif z < self._z_0:
+            new_z_0 = z
+
+        newBuffer = Buffer(new_x_0, new_y_0, new_z_0, new_x, new_y, new_x)
+
+        self.write(newBuffer)
+
+        return newBuffer
 
     def setOrigin(self, O):
-        if isinstance(O, Vector):
-            self._O = O.toList()
-        else:
-            self._O = O
+        self._x_0 = O[0]
+        self._y_0 = O[1]
+        self._z_0 = O[2]
 
     def get(self, x, y, z):
-        return self._arr[x][y][z]
+        return self._arr[x - self._x_0][y - self._y_0][z - self._z_0]
 
     def getOrigin(self):
-        return self._O
+        return [self._x_0, self._y_0, self._z_0]
+
+    def getShape(self):
+        return self._arr.shape
 
     def write(self, other, ignoreAir=True):
         if isinstance(other, Buffer):
-            x_len, y_len, z_len = self._arr.shape
-
             id = 0
 
-            for x in range(0,x_len):
-                for y in range(0,y_len):
-                    for z in range(0,z_len):
+            for x in range(self._x_0, self._x_1):
+                for y in range(self._y_0, self._y_1):
+                    for z in range(self._z_0, self._z_1):
                         id = self.get(x,y,z)
 
                         if ignoreAir and id == 0:
                             continue
                         
-                        other.set(x + self._O[0],y + self._O[1],z + self._O[2], id)
+                        other.set(x ,y, z, id)
 
 class GameBuffer(Buffer):
-    def __init__(self, mc):
+    def __init__(self, mc, Zero):
         self.mc = mc
+        self._x_offset = Zero[0]
+        self._y_offset = Zero[1]
+        self._z_offset = Zero[2]
 
     def set(self,x,y,z,id):
-        # msg = "Set block at ", str(x) ,", ", str(y), ",", str(z), "to ", 0
-        # self.mc.postToChat(msg)
-        self.mc.setBlock(x,y,z, id)
+        msg = "Set block at ", str(x) ,", ", str(y), ",", str(z), "to ", id
+        self.mc.postToChat(msg)
+        self.mc.setBlock(x + self._x_offset,y + self._y_offset,z + self._z_offset, id)
 
     def get(self,x,y,z):
-        return self.mc.getBlock(x,y,z)
+        return self.mc.getBlock(x + self._x_offset,y + self._y_offset,z + self._z_offset)
--- pcglib/Main.py
+++ pcglib/Main.py
@@ -7,12 +7,15 @@ from Primitives import Cuboid
 from Buffer import Buffer
 from Buffer import GameBuffer
 
-game = GameBuffer(mc)
+game = GameBuffer(mc, Zero)
 
-x_len,y_len,z_len = 10,10,10
 
-buf1 = Buffer(x_len, y_len, z_len)
+buf1 = Buffer(0,60,0, 10,70,10)
 
-buf1.setOrigin(Zero + [0, 70, 0])
+len_x, len_y, len_z = buf1.getShape()
+
+buf1.set(0, 69, 0, 1)
+
+print(buf1.get(0, 69, 0))
 
 buf1.write(game)
