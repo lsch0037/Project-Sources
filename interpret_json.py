2dea1b448069fa6f0a8d527e41bdc61e5554f2a6
BuildTools <lukas.schimpf@gmail.com>
2023-03-03 12:02:45 +0100

    Strict reliance on properties

--- interpret_json.py
+++ interpret_json.py
@@ -1,6 +1,8 @@
 import sys
 import json
 
+import numpy as np
+
 from pcglib.vec3 import vec3
 from pcglib.mat4 import mat4
 from pcglib.Game import Game
@@ -38,39 +40,36 @@ f.close()
 prog = json.loads(text)
 
 def parse_program(prog):
-    print("Parsing Program:", prog)
     tree = compound()
     props = {
-        "Position" : [0.5,100.5,0.5]
+        "Position" : [0.5, 100.5, 0.5]
     }
 
-    # for key in prog:
+    # Parse an expression
     node = parse_expression(prog,props)
     tree.addChild(node)
         
     return tree
 
-def parse_expression(expr, old_props):
+def parse_expression(expr, parent_props):
+
     print("Parsing Expression:", expr)
-
-    new_props = old_props.copy()
-
-    if "Props" in prog:
-        new_props = parse_props(prog["Props"], old_props)
-
+    props = parse_props(expr, parent_props)
+    print("Props:", props)
 
     # If expression is a primitive
     if "Shape" in expr:
-        return parse_primitive(expr, new_props)
+        return parse_primitive(expr, props)
     
     # If expression is an operator
     else:
-        return parse_operator(expr, new_props)
+        return parse_operator(expr, props)
 
 
 def parse_primitive(prog, props):
 
-    print("Parsing Primitive:", prog, ", Props:", props)
+    print("Parsing Primitive:", prog)
+    print("Props:", props)
     shape = prog["Shape"]
 
     if shape == "Cube":
@@ -99,31 +98,40 @@ def parse_operator(operator, props):
     # TODO FOR OTHER OPERATORS
 
 
-def parse_cube(prog,props):
+def parse_cube(prog,parent_props):
+    props = parse_props(prog,parent_props)
     print("Parsing Cube:", prog)
-    size = prog["Size"]
-    material = get_material(prog, props)
-    pos = get_position(prog, props)
+    print("Props:", props)
+
+    size = props["Size"]
+    material = props["Material"]
+    pos = props["Position"]
 
     print("cube:",pos, idMat, material, size)
+    print("Cube(pos:",pos,", Material:", material,"Size:",size,")")
 
     return cube(pos, idMat, material, size)
 
 
 def parse_sphere(prog,props):
     print("Parsing Sphere:", prog)
-    rad = prog["Radius"]
-    material = get_material(prog, props)
-    pos = get_position(prog, props)
+    print("Props:", props)
 
-    return sphere(pos, idMat, material, rad)
+    rad = props["Radius"]
+    material = props["Material"]
+    pos = props["Position"]
+
+    print("Sphere(pos:",pos,", Material:", material, ",Radius:", rad,")")
+
+    return sphere(pos, material, rad)
+
 
 def parse_cylinder(prog, props):
     print("Parsing Cylinder:", prog)
-    rad = prog["Radius"]
-    len = prog["Length"]
-    material = get_material(prog, props)
-    pos = get_position(prog, props)
+    rad = props["Radius"]
+    len = props["Length"]
+    material = props["Material"]
+    pos = props["Position"]
 
     return cylinder(pos, idMat, material, rad, len)
 
@@ -134,14 +142,12 @@ def parse_union(prog, props):
     print("Parsing Union:", prog)
     union_node = unionNode()
 
-    for key in prog:
-        if key == "Props":
-            continue
-
-        node = parse_expression(prog[key], props)
+    for item in prog:
+        node = parse_expression(item, props)
         union_node.addChild(node)
 
     return union_node
+
 
 def get_material(prog, props):
     mat = str()
@@ -159,37 +165,48 @@ def get_position(prog, props):
 
     if "Absolute" in prog:
         pos = prog["Absolute"]
-        return vec3(pos)
+        return np.array(pos)
 
     elif "Relative" in prog:
-        rel = prog["Relative"]
-        pos = props["Position"]
-        return vec3(pos) + vec3(rel)
+        rel = np.array(prog["Relative"])
+        pos = np.array(props["Position"])
+        return np.add(pos,rel)
 
     else:
         pos = props["Position"]
-        return vec3(pos)
+        return np.array(pos)
 
 
-def parse_props(prog, props):
-    print("Parsing Properties", prog)
+def parse_props(prog, parent_props):
 
-    new_props = props.copy()
+    props = parent_props.copy()
+
+    for key in prog:
+        if key in operatorNames:
+            continue
+
+        elif key == "Absolute" or key == "Relative":
+            continue
+
+        props[key] = prog[key]
+
 
     # POSITION PROPERTIES
     if "Absolute" in prog:
-        new_props["Position"] = prog["Absolute"]
+        props["Position"] = prog["Absolute"]
     
     elif "Relative" in prog:
-        new_props["Position"] = props["Position"] + prog["Relative"]
+        pos = np.array(props["Position"])
+        rel = np.array(prog["Relative"])
+        props["Position"] = pos + rel
 
-    # MATERIAL PROPERTIES
-    if "Material" in prog:
-        new_props["Material"] = prog["Material"]
+    return props
 
-    # print("Inner Properties:", new_props)
+def parse_variable(prog, props):
+    pass
 
-    return new_props
+def parse_function(prog,props):
+    pass
 
 
 # TODO PARSE OTHER OPERATORS
@@ -197,7 +214,7 @@ def parse_props(prog, props):
 
 tree = parse_program(prog)
 
-zero_offset = vec3([-144, -81, -224])
-game = Game(zero_offset)
+# zero_offset = vec3([-144, -81, -224])
+# game = Game(zero_offset)
 
-tree.set(game)
+# tree.set(game)
