3ccf5d8e7f97a97b2e819b55d92cb312688b9ac3
BuildTools <lukas.schimpf@gmail.com>
2023-03-28 17:17:45 +0200

    Simple sentence parsing

--- interpret_input.py
+++ interpret_input.py
@@ -4,12 +4,12 @@ from os import path
 import json
 
 # !Importing nlp library
-import spacy
+# import spacy
 
-nlp = spacy.load("en_core_web_sm")
+# nlp = spacy.load("en_core_web_sm")
 
 descriptors = ["Big", "Small", "Large", "Tall", "Short"]
-modifiers = ["on","on top of", "next to", "under"]
+modifiers = ["on", "on top of", "next to", "under", "in", "south of", "north of", "east of", "west of"]
 
 objects = dict()
 meta = dict()
@@ -44,68 +44,36 @@ def check_files():
         objects[objNameCleaned] = objJson
         meta[objNameCleaned] = metaJson
     
+# def tokensise(text):
+#     tokens = []
 
-# def parse_literal(token, expected):
-#     if token == expected:
-#         return
-#     else:
-#         raise ValueError("Expected literal:{}".format(expected))
+#     sentences = text.split(".")
+#     for sentence in sentences:
 
+#         phrases = sentence.split(",")
+#         phrase_tkns = []
+#         for phrase in phrases:
 
-# def parse_verb(token):
-
-#     if token in verbs:
-#         return token
-#     else:
-#         return None
-
-
-# def parse_noun(tokens, ctr):
-#     pathName = 'json\\'+tokens[ctr]+'.json'
-    
-#     if path.exists(pathName):
-#         ctr += 1
-#         return json.loads(pathName)
-#     else:
-#         raise ValueError("No such file : '{}'".format(pathName))
-
-
-# def parse_sentence(tokens, ctr):
-#     parse_literal(tokens[0], "A")
-
-#     verb = parse_verb(tokens[1])
-
-#     noun = parse_noun(tokens, ctr)
-#     parse_literal('.', tokens, ctr)
-
-#     return createObject(noun,verb)
-
-def tokensise(text):
-    tokens = []
-
-    sentences = text.split(".")
-    for sentence in sentences:
-
-        phrases = sentence.split(",")
-        phrase_tkns = []
-        for phrase in phrases:
-
-            words = phrase.split()
-            phrase_tkns.append(words)
+#             words = phrase.split()
+#             phrase_tkns.append(words)
         
-        tokens.append(phrase_tkns)
+#         tokens.append(phrase_tkns)
 
-    return tokens
+#     return tokens
 
 
 def parse_paragraph(paragraph):
-    sentences = paragraph.split('.')
+    print("Parsing paragraph: '{}'".format(paragraph))
+    sentences = paragraph.split(".")
+    print("Sentences:",sentences)
+
+    if len(sentences) < 3:
+        return parse_sentence(sentences[0])
 
     sentence_progs = []
     for sentence in sentences:
         sentence_prog = parse_sentence(sentence)
         sentence_progs.append(sentence_prog)
-    
 
     prog = dict()
 
@@ -114,25 +82,12 @@ def parse_paragraph(paragraph):
     return prog
 
 def parse_sentence(sentence):
-    # doc = nlp(sentence)
-    # obj_type = None
-    # obj_attrs = []
-    
-    # for token in doc:
-    #     tkn_text = token.text.title()
-    #     if tkn_text in objects:
-    #         obj_type = tkn_text
-    #     elif tkn_text in verbs:
-    #         obj_attrs.append(tkn_text)
-
-    # print("Objects:", obj_type)
-    # print("Attributes:",obj_attrs)
-
-    # prog = eval_object(obj_type, obj_attrs)
-
-    # return prog
+    print("Parsing sentence: '{}'".format(sentence))
 
     clauses = sentence.split(',')
+
+    if len(clauses) == 1:
+        return parse_clause(clauses[0])
 
     clause_progs = []
     for clause in clauses:
@@ -143,18 +98,65 @@ def parse_sentence(sentence):
     prog["Union"] = clause_progs
     return prog
 
+
 def parse_clause(clause):
+    print("Parsing clause: '{}'".format(clause))
+
+    first_mod_index = len(clause)
+    first_mod = None
+    
+    for m in modifiers:
+        index = clause.rfind(m)
+        if not index == -1 and index < first_mod_index:
+            first_mod_index = index
+            first_mod = m
+
+    if first_mod == None:
+        return parse_object(clause)
+
+    else:
+        # print("Evaluating modifier '{}'".format(first_mod))
+        # splits = clause.split(first_mod, 1)
+        first_clause, mod, second_clause = clause.partition(first_mod)
+        
+        first_obj = parse_object(first_clause)
+        mod = parse_modifier(second_clause, first_mod)
+        second_obj = parse_clause(second_clause)
+
+        return eval_modifier(first_obj, mod, second_obj)
+
+# Return the modifier defined on the primary object of the clause given
+def parse_modifier(clause, modifier):
+    print("Evaluating modifier '{}'".format(modifier))
     words = clause.split()
 
-    obj_description = []
+    obj = None
 
-    for i in range(len(words)):
-        if words[i] in modifiers:
-            # TODO parse 2 word modifiers like "next to"
-            pass
+    for word in words:
+        if word in objects:
+            obj = word
 
-        else:
-            obj_description.append(words[i])
+    return meta[obj]["Modifiers"][modifier]
+
+
+# Return the program for the object with the given descriptors
+def parse_object(text):
+    words = text.split()
+
+    desc = []
+    current_object = None
+    for word in words:
+        if word.lower() in descriptors:
+            desc.append(word.lower())
+
+        elif word.title() in objects:
+            current_object = word.title()
+            break
+
+    if current_object == None:
+        raise ValueError("Object description '{}' does not contain an object type.".format(text))
+
+    return eval_object(current_object, desc)
 
 def eval_attributes(object_name, object_attributes):
     meta_attributes = meta[object_name]["Attributes"]
@@ -205,10 +207,6 @@ text = f.read()
 f.close()
 
 # TOKENISE
-# tokens = tokensise(text)
-# print("Tokens:", tokens)
-
-# prog = parse_sentence(text)
 prog = parse_paragraph(text)
 
 # Write to 'Prog.json'
